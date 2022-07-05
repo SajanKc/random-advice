02c8a158fa48cd4e3f42f63cc31cf93554db837b
Sajan Kc <sajan1996kc@gmail.com>
2022-07-05 21:15:50 +0545

    no data render fixed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ function App() {
 	const baseUrl = "https://api.adviceslip.com/advice";
 	const [advice, setAdvice] = useState([]);
 
+	useEffect(() => {
+		getAdvice();
+	}, []);
+
 	const getAdvice = () => {
 		axios.get(baseUrl)
 			.then((response) => {
@@ -17,18 +21,27 @@ function App() {
 			});
 	};
 
-	useEffect(() => {
-		getAdvice();
-	}, []);
+	const reloadPage = () => {
+		window.location.reload();
+	};
 
 	return (
 		<div className="app">
-			<div className="card">
-				<h1 className="heading">{advice}</h1>
-				<button className="btn_advice" onClick={getAdvice}>
-					Give Me Advice!
-				</button>
-			</div>
+			{advice.length !== 0 ? (
+				<div className="card">
+					<h1 className="heading">{advice}</h1>
+					<button className="btn_advice" onClick={getAdvice}>
+						Give Me Advice!
+					</button>
+				</div>
+			) : (
+				<div className="card">
+					<h1 className="heading">Something Went Wrong.</h1>
+					<button className="btn_advice" onClick={reloadPage}>
+						Refresh
+					</button>
+				</div>
+			)}
 		</div>
 	);
 }
